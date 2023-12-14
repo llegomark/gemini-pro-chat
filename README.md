c7d01ff9e966d833242ac5e480416278e5426718
Mark Anthony Llego <markllego@gmail.com>
2023-12-14 10:11:09 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,64 @@
+## Gemini Pro Chat Application
+
+This Python application utilizes the Gemini Pro API from `google.generativeai` to create an interactive chat interface. It allows users to have a conversation with the Gemini Pro AI model, with features like viewing chat history, restarting chats, and automatically saving chat sessions to a text file with timestamps.
+
+## Installation
+
+### Prerequisites
+
+- Python 3.x
+- Pip package manager
+- An API key for Gemini Pro
+
+### Setup
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/llegomark/gemini-pro-chat.git
+   cd gemini-pro-chat
+   ```
+
+2. Install required Python packages:
+   ```bash
+   pip install google-generativeai python-dotenv
+   ```
+
+3. Create a `.env` file in the project directory and add your Gemini Pro API key:
+   ```
+   GEMINI_API_KEY=your_api_key_here
+   ```
+
+## Usage
+
+Run the application using Python:
+
+```bash
+python chat.py
+```
+
+### Commands
+
+- Type your message and press Enter to chat with Gemini Pro.
+- Type `history` to view the complete chat history.
+- Type `restart` to start a new chat session while saving the current session to `chat_history.txt`.
+- Type `exit` to exit the application and save the current session.
+
+## Features
+
+- Interactive chat with Gemini Pro AI.
+- View chat history within the application.
+- Automatically saves chat sessions with timestamps.
+- Supports restarting chat sessions while preserving history.
+- Streaming output for a more natural chat experience.
+
+## Contributing
+
+Contributions to the Gemini Pro Chat Application are welcome. Please feel free to fork the repository, make improvements, and submit pull requests.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+- Google's Gemini API for providing the language model. More information about the Gemini API can be found in [Google's official announcement](https://blog.google/technology/ai/gemini-api-developers-cloud/).
---
+++ chat.py
@@ -0,0 +1,87 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+
+def main():
+    load_dotenv()
+    api_key = os.getenv("GEMINI_API_KEY")
+
+    genai.configure(api_key=api_key)
+
+    global_history = []
+    last_saved_index = 0
+
+    while True:
+        try:
+            model = genai.GenerativeModel('gemini-pro')
+            chat = model.start_chat(history=[])
+
+            last_saved_index = add_to_history_and_save(
+                global_history, "Session started", "", last_saved_index, new_session=True)
+
+            while True:
+                user_input = input("User: ").strip().lower()
+
+                if user_input == "history":
+                    display_chat_history(global_history)
+                    continue
+
+                if user_input == "restart":
+                    save_chat_to_file(
+                        global_history[last_saved_index:], "chat_history.txt")
+                    last_saved_index = len(global_history)
+                    break
+
+                if user_input == "exit":
+                    save_chat_to_file(
+                        global_history[last_saved_index:], "chat_history.txt")
+                    return
+
+                if user_input:
+                    response = chat.send_message(user_input, stream=True)
+                    response_text = ""
+                    for chunk in response:
+                        response_text += chunk.text
+                        print(chunk.text)
+
+                    last_saved_index = add_to_history_and_save(
+                        global_history, user_input, response_text, last_saved_index)
+                else:
+                    print("Please enter some text.")
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
+def add_to_history_and_save(history, user_input, response_text, last_saved_index, new_session=False):
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    if new_session:
+        history.append(
+            {'role': 'system', 'text': "--- New Session ---", 'timestamp': timestamp})
+    history.extend([
+        {'role': 'user', 'text': user_input, 'timestamp': timestamp},
+        {'role': 'gemini', 'text': response_text, 'timestamp': timestamp}
+    ])
+    save_chat_to_file(history[last_saved_index:], "chat_history.txt")
+    return len(history)
+
+
+def display_chat_history(history):
+    print("\nComplete Chat History:")
+    for message in history:
+        sender = "User" if message['role'] == 'user' else "Gemini"
+        print(f"{message['timestamp']} {sender}: {message['text']}")
+    print("\n")
+
+
+def save_chat_to_file(history, filename):
+    with open(filename, "a", encoding="utf-8") as file:
+        for message in history:
+            sender = "User" if message['role'] == 'user' else "Gemini"
+            file.write(f"{message['timestamp']} {sender}: {message['text']}\n")
+
+
+if __name__ == "__main__":
+    main()
